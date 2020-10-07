3b83c68490d2b2b80103b96dbeea864090b60c29
B3T4C0R3 <B3T4C0R3@users.noreply.github.com>
2020-10-07 16:55:25 +0000

    deploy: B3T4C0R3/IGUS@1643f4c679367e1371441bde854bf5d81c2aa8eb

---
+++ text/brand.js
@@ -0,0 +1,16 @@
+const brand = {
+  igus: {
+    name: 'IG',
+    desc: 'Presidential Election Trading Days - Powered by IG',
+    prefix: 'IG',
+    footerText: 'IG',
+    logoText: 'Presidential Election Trading Days - Powered by IG',
+    projectName: 'Presidential Election Trading Days - Powered by IG',
+    url: 'igus.nl',
+    img: '/static/images/igus-logo.png',
+    notifMsg:
+      'Donec sit amet nulla sed arcu pulvinar ultricies commodo id ligula.'
+  }
+}
+
+export default brand
