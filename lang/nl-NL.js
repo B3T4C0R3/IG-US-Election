67f6739d09961c2de5478e8723f5d86d4819bab2
B3T4C0R3 <B3T4C0R3@users.noreply.github.com>
2020-10-16 08:13:58 +0000

    deploy: B3T4C0R3/IGUS@26ef34725f2d2ded78d4c02e1ca28e1b726ebbae

--- lang/nl-NL.js
+++ lang/nl-NL.js
@@ -89,7 +89,9 @@ export default () => {
         caption: 'Preview Wereldoverzicht',
         TOS_title: 'Algemene voorwaarden US Elections Training Week',
         TOS_text:
-          "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software including versions of Lorem Ipsum."
+          "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software including versions of Lorem Ipsum.",
+        disclaimer:
+          'Opties en turbocertificaten zijn complexe financiële instrumenten. Uw vermogen loopt risico. U kunt uw geld snel verliezen.'
       }
     })
   })
--- text/brand.js
+++ text/brand.js
@@ -1,9 +1,9 @@
 const brand = {
   igus: {
-    name: 'IG',
+    name: 'IG Europe',
     desc: 'Presidential Election Training Week - Powered by IG',
-    prefix: 'IG',
-    footerText: 'IG',
+    prefix: 'IG Europe',
+    footerText: 'IG Europe',
     logoText: 'Presidential Election Training Week - Powered by IG',
     projectName: 'Presidential Election Training Week - Powered by IG',
     url: 'b3t4c0r3.github.io',
